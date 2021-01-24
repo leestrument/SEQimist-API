bae2a28dc94640f712a0dd1b92900d952909845b
이혁기 <leestrument@hanmail.net>
2021-01-25 02:22:23 +0900

    mixer is done

--- core/track/seq-track-mixer.ts
+++ core/track/seq-track-mixer.ts
@@ -2,9 +2,55 @@ export class Mixer {
 
     constructor(
 
-        public _isMuted = false,
-        public _isSolo = false,
+        private _isActive   = true,
+        private _isSolo     = false,
 
     ){}
+
+    // setter
+    public active(): void { 
+        
+        this._isActive = true 
+    
+    }
+    public mute(): void   { 
+        
+        this._isActive = false 
+    
+    }
+    public solo(): void {
+
+        this._isSolo = true
+
+    }
+    public unsolo(): void {
+
+        this._isSolo = false
+
+    }
+
+    // getter
+    public isActive(): boolean {
+
+        return this._isActive === true
+
+    }
+    public isMuted(): boolean {
+
+        return this._isActive === false
+
+    }
+    public isSolo(): boolean {
+
+        return this._isSolo
+
+    }
+    public isOn(): boolean {
+
+        // isOn dedicates active + solo state.
+
+        return  this.isActive() || this.isSolo()
+
+    }
 
 }
--- core/track/seq-track.ts
+++ core/track/seq-track.ts
@@ -19,4 +19,10 @@ export class Track {
         
     ){}
 
+    public getMixer() {
+
+        return this._mixer
+
+    }
+
 }
