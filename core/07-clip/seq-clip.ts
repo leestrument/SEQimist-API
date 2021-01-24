d48abf1fdcd6633baf73cdcc5b1e713ffa72a628
이혁기 <leestrument@hanmail.net>
2021-01-25 07:53:03 +0900

    Added constant for visible track count

--- core/07-clip/seq-clip.ts
+++ core/07-clip/seq-clip.ts
@@ -2,6 +2,10 @@ import { Interval }     from './seq-clip-interval'
 import { ClipPreset }   from './seq-clip-preset'
 import { Track }        from '../06-track/seq-track'
 
+export const SEQ_VISIBLE_TRACK_COUNT_MIN   = 1
+export const SEQ_VISIBLE_TRACK_COUNT_MAX   = 64
+export const SEQ_VISIBLE_TRACK_COUNT_DEF   = 8
+
 export class Clip {
 
     constructor(
@@ -10,8 +14,8 @@ export class Clip {
         private _color              = 0,
         private _interval           = new Interval,
         private _preset             = new ClipPreset,
-        private _visibleTrackCount  = 8,
-        private _tracks             = Array.from({length : 64}, () => new Track),
+        private _visibleTrackCount  = SEQ_VISIBLE_TRACK_COUNT_DEF,
+        private _tracks             = Array.from({length : SEQ_VISIBLE_TRACK_COUNT_MAX}, () => new Track),
 
     ){}
 
