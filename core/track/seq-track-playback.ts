20058fbc047bdde6281916acf3ca688b53d18e7c
이혁기 <leestrument@hanmail.net>
2021-01-25 02:28:48 +0900

    updated playback class

--- core/track/seq-track-playback.ts
+++ core/track/seq-track-playback.ts
@@ -1,11 +1,31 @@
+const PLAY_BACK_LENGTH_DEF  = 1
+const PLAY_BACK_SPEED_DEF   = 1
+const PLAY_BACK_SWING_DEF   = 0
+
 export class PlayBack {
 
     constructor(
 
-        public _length = 1, 
-        public _speed  = 1, 
-        public _swing  = 0,
+        public _length = PLAY_BACK_LENGTH_DEF, 
+        public _speed  = PLAY_BACK_SPEED_DEF, 
+        public _swing  = PLAY_BACK_SWING_DEF,
 
     ){}
+
+    public setLength(length: number): void {
+
+        this._length = length
+
+    }
+    public setSpeed(speed: number): void {
+
+        this._speed = speed
+
+    }
+    public setSwing(swing: number): void {
+
+        this._swing = swing
+
+    }
 
 }
