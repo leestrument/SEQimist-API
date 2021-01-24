6938fcffd0a116739fbfdb4e4d3d64c5692a5b4f
이혁기 <leestrument@hanmail.net>
2021-01-25 02:48:38 +0900

    Track prefix is added for constant

--- core/track/seq-track-loop.ts
+++ core/track/seq-track-loop.ts
@@ -1,11 +1,11 @@
 import { LeeRange } from '../../util/lee-range'
 
-const LOOP_MIN_DEF      = 0
-const LOOP_MAX_DEF      = 1
-const LOOP_ACTIVE_DEF   = false
+const TRACK_LOOP_MIN_DEF      = 0
+const TRACK_LOOP_MAX_DEF      = 1
+const TRACK_LOOP_ACTIVE_DEF   = false
 
 export class TrackLoop extends LeeRange {
 
-    constructor(){ super(LOOP_MIN_DEF, LOOP_MAX_DEF, LOOP_ACTIVE_DEF) }
+    constructor(){ super(TRACK_LOOP_MIN_DEF, TRACK_LOOP_MAX_DEF, TRACK_LOOP_ACTIVE_DEF) }
 
 }
