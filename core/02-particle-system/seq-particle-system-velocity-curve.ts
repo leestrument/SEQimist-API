f5fbc6925ece27f9fd248ffc1d652f7b5202ac6f
이혁기 <leestrument@hanmail.net>
2021-01-25 06:42:19 +0900

    Implemented Velocity Curve

--- core/02-particle-system/seq-particle-system-velocity-curve.ts
+++ core/02-particle-system/seq-particle-system-velocity-curve.ts
@@ -1,12 +1,29 @@
 import { LeeRange }             from '../../util/lee-range'
 import { VelocityCurveType }    from './seq-particle-system-velocity-curve-type'
 
+export const SEQ_VELOCITY_CURVE_MIN = 1
+export const SEQ_VELOCITY_CURVE_MAX = 127
+
 export class VelocityCurve extends LeeRange {
 
     constructor(
 
         private _type = VelocityCurveType.Up
 
-    ){ super() }
+    ){ super(SEQ_VELOCITY_CURVE_MIN, SEQ_VELOCITY_CURVE_MAX) }
+
+    // setter
+    public setType(type: VelocityCurveType): void {
+
+        this._type = type
+
+    }
+
+    // getter
+    public getType(): VelocityCurveType {
+
+        return this._type
+
+    }
 
 }
