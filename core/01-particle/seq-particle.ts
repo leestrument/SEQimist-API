0d20d5554788f068f3bccd2d513403e71ff8dcc8
이혁기 <leestrument@hanmail.net>
2021-01-25 06:08:33 +0900

    Implemented Particle

--- core/01-particle/seq-particle.ts
+++ core/01-particle/seq-particle.ts
@@ -1,10 +1,40 @@
+const PARTICLE_VELOCITY_MIN = 1 
+const PARTICLE_VELOCITY_MAX = 127
+const PARTICLE_VELOCITY_DEF = 64
+const PARTICLE_GATE_MIN = 0
+const PARTICLE_GATE_MAX = 1
+
 export class Particle {
 
     constructor(
 
-        private _velocity   = 64,
-        private _gate       = 1,
+        private _velocity   = PARTICLE_VELOCITY_DEF,
+        private _gate       = PARTICLE_GATE_MAX,
 
     ){}
+
+    // setter
+    public setVelocity(velocity: number): void {
+
+        this._velocity = velocity
+
+    }
+    public setGate(gate: number): void {
+
+        this._gate = gate
+
+    }
+
+    // getter
+    public getVelocity(): number {
+
+        return this._velocity
+
+    }
+    public getGate(): number {
+
+        return this._gate
+
+    }
 
 }
