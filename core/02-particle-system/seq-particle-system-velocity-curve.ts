330fd843c1e5d3792e85ad87d0fa26131553ef46
이혁기 <leestrument@hanmail.net>
2021-01-25 06:36:08 +0900

    Introduce VelocityCurveType

--- core/02-particle-system/seq-particle-system-velocity-curve.ts
+++ core/02-particle-system/seq-particle-system-velocity-curve.ts
@@ -1,10 +1,11 @@
-import { LeeRange } from '../../util/lee-range'
+import { LeeRange }             from '../../util/lee-range'
+import { VelocityCurveType }    from './seq-particle-system-velocity-curve-type'
 
 export class VelocityCurve extends LeeRange {
 
     constructor(
 
-        private _type = 'linear'
+        private _type = VelocityCurveType.Up
 
     ){ super() }
 
