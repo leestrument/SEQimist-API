2de3fb139167e9f28bfc4278d68439af2be128be
이혁기 <leestrument@hanmail.net>
2021-01-25 07:05:07 +0900

    Implemented Note Class, but still need more works

--- core/03-note/seq-note.ts
+++ core/03-note/seq-note.ts
@@ -2,18 +2,80 @@ import { PitchBend }        from './seq-note-pitchBend'
 import { NoteRandomizer }   from './seq-note-randomizer'
 import { ParticleSystem }   from '../02-particle-system/seq-particle-system'
 
+const SEQ_NOTE_IS_SELECTED_DEF  = false
+const SEQ_NOTE_IS_ACTIVE_DEF    = true
+
 export class Note {
 
     constructor(
 
         private _start          : number,
         private _duration       : number,
-        private _isSelected     = false, 
-        private _isActive       = true,
+        private _isSelected     = SEQ_NOTE_IS_SELECTED_DEF, 
+        private _isActive       = SEQ_NOTE_IS_ACTIVE_DEF,
         private _pitchBend      = new PitchBend, 
         private _randomizer     = new NoteRandomizer,
         private _particleSystem = new ParticleSystem,
 
     ){}
+
+    // setter
+    public setStart(start: number): void {
+
+        this._start = start
+
+    }
+    public setDuration(duration: number): void {
+
+        this._duration = duration
+
+    }
+    public select(): void {
+
+        this._isSelected = true
+
+    }
+    public deselect(): void {
+
+        this._isSelected = false
+
+    }
+    public active(): void {
+
+        this._isActive = true
+
+    }
+    public mute(): void {
+
+        this._isActive = false
+
+    }
+
+    // getter
+    public getStart(): number {
+
+        return this._start
+
+    }
+    public getDuration(): number {
+
+        return this._duration
+
+    }
+    public isSelected(): boolean {
+
+        return this._isSelected
+
+    }
+    public isActive(): boolean {
+
+        return this._isActive
+
+    }
+    public isMuted(): boolean {
+
+        return this._isActive === false
+
+    }
 
 }
