778b42071586a9612a7fc58b77e188316ee055bd
이혁기 <leestrument@hanmail.net>
2021-01-26 13:58:37 +0900

    Added Def constant on velocity curve

--- core/02-atomBox/seq-atombox-velocityCurve.ts
+++ core/02-atomBox/seq-atombox-velocityCurve.ts
@@ -1,13 +1,22 @@
 import { LeeRange } from '../../util/lee-range'
 import { SEQ_ATOM_VELOCITY_MIN, SEQ_ATOM_VELOCITY_MAX } from '../01-atom/seq-atom'
 
-export enum     VelocityCurveType       { Up, Down, Random, Fixed }
+export enum VelocityCurveType { 
+    
+    Up, 
+    Down, 
+    Random, 
+    Fixed 
+
+}
+
+const SEQ_VELOCITY_CURVE_DEF = VelocityCurveType.Up
 
 export class VelocityCurve extends LeeRange {
 
     constructor(
 
-        private _type = VelocityCurveType.Up
+        private _type = SEQ_VELOCITY_CURVE_DEF
 
     ){ super(SEQ_ATOM_VELOCITY_MIN, SEQ_ATOM_VELOCITY_MAX) }
 
